dea74dd2d5467120ecabcb8346c478eaf75c7c50
marcusyhchen <tronsyuyu@gmail.com>
2022-10-25 19:23:37 +0800

    add route file to route

---
+++ pkg/app/server/route.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"cloud-native-c/pkg/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+
+	router.GET("/hello", hello)
+
+	// .. //
+	user := router.Group("/user") //本人
+	user.POST("/deposit", controllers.Deposit)
+	user.POST("/withdrawal", controllers.Withdraw)
+
+	users := router.Group("/users")
+	users.POST("/:id/deposit", controllers.DepositTo)
+	users.POST("/:id/withdrawal", controllers.WithdrawFrom)
+
+	return router
+}
+
+func hello(c *gin.Context) {
+	c.JSON(200, gin.H{ // response json
+		"message": "hello world",
+	})
+}
--- pkg/app/server/server.go
+++ pkg/app/server/server.go
@@ -2,14 +2,9 @@ package server
 
 import (
 	"cloud-native-c/pkg/config"
-	"cloud-native-c/pkg/controllers"
 	"cloud-native-c/pkg/db"
 	"log"
-
-	"github.com/gin-gonic/gin"
 )
-
-const confPath = "./config.json"
 
 type Server struct {
 	Config *config.Config
@@ -33,28 +28,4 @@ func (t *Server) Serve() {
 	log.Printf("======= Server start to listen (%s) and serve =======\n", addr)
 	r := Router()
 	r.Run(addr)
-}
-
-func Router() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.New()
-
-	router.GET("/hello", hello)
-
-	// .. //
-	user := router.Group("/user") //本人
-	user.POST("/deposit", controllers.Deposit)
-	user.POST("/withdrawal", controllers.Withdraw)
-
-	users := router.Group("/users")
-	users.POST("/:id/deposit", controllers.DepositTo)
-	users.POST("/:id/withdrawal", controllers.WithdrawFrom)
-
-	return router
-}
-
-func hello(c *gin.Context) {
-	c.JSON(200, gin.H{ // response json
-		"message": "hello world",
-	})
 }
