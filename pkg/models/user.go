498c10919827a24c7fef4b8d5914ed9397f10ca0
marcusyhchen <tronsyuyu@gmail.com>
2022-10-25 16:49:55 +0800

    add column 'balance' to user in DB'

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -4,4 +4,5 @@ type User struct {
 	GormModel
 	Name     string `gorm:"unique;type:varchar(128)"` //trons varchar(255)
 	Password string `gorm:"type:char(64)"`
+	Balance  uint64 `gorm:"type:bigint;not null;default:0"` //need default whend add column and type:not null
 }
